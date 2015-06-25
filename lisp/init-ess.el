265d9b4e7a021a36022853d8320bba37df3548ab
Jeffrey Arnold <jrnold@turk>
2015-06-25 12:11:10 -0700

    ess use indent of 2

--- lisp/init-ess.el
+++ lisp/init-ess.el
@@ -4,37 +4,28 @@
 ;; The indentation style is from the official R coding standards
 ;; R-core uses C++ http://www.cran.r-project.org/doc/manuals/R-ints.html#R-coding-standards
 ;; hadley uses 2 spaces.
+;;                                 DEF GNU BSD K&R C++
+;; ess-indent-level                  2   2   8   5   4
+;; ess-continued-statement-offset    2   2   8   5   4
+;; ess-brace-offset                  0   0  -8  -5  -4
+;; ess-arg-function-offset           2   4   0   0   0
+;; ess-expression-offset             4   2   8   5   4
+;; ess-else-offset                   0   0   0   0   0
+;; ess-close-brace-offset            0   0   0   0   0
 
-	  '(lambda()
-	     (setq ess-indent-level 2
-		   ess-continued-statement-offset 2
-		   ess-
-	     
-	     
-	     ;; Because
-	     ;;                                 DEF GNU BSD K&R C++
-	     ;; ess-indent-level                  2   2   8   5   4
-	     ;; ess-continued-statement-offset    2   2   8   5   4
-	     ;; ess-brace-offset                  0   0  -8  -5  -4
-	     ;; ess-arg-function-offset           2   4   0   0   0
-	     ;; ess-expression-offset             4   2   8   5   4
-	     ;; ess-else-offset                   0   0   0   0   0
-	     ;; ess-close-brace-offset            0   0   0   0   0
-             ))
+(add-hook 'ess-mode-hook
+	  (lambda()
+	    (setq ess-indent-level 2
+		  tab-width 2)))
+
 ;; No more _ to <-
 (ess-toggle-underscore nil)
+
 ;; no more fancy comments
 (setq ess-fancy-comments nil)
 
 (add-hook 'Rnw-mode-hook 
 	  (lambda () (setq ispell-parser 'tex)))
-
-(defun r-scratch (buffer)
-  "Open a buffer to do R work in"
-  (interactive "BBuffer name: ")
-  (let ((buffer-or-name (or buffer "*R-scratch*")))
-    (switch-to-buffer buffer-or-name)
-    (r-mode)))
 
 ;; Update ispell-latex to ignore Rnw
 ;; TODO
